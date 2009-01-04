c33727aa6b879669729362932e0586efda2ab49a
cesar@forzaframework.org <cesar@forzaframework.org@6f6ecb54-d94e-11dd-86c2-0f8696b7b6f9>
2009-01-04 19:15:51 +0000

    Lincese

--- trunk/core/java/src/org/forzaframework/beans/propertyeditors/CustomClassEditor.java
+++ trunk/core/java/src/org/forzaframework/beans/propertyeditors/CustomClassEditor.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.forzaframework.beans.propertyeditors;
 
 import org.springframework.util.StringUtils;
@@ -11,19 +27,6 @@ import java.io.Serializable;
  * @author cesarreyes
  *         Date: 14-ago-2008
  *         Time: 16:20:02
- */
-/**
- * Editor for <code>java.lang.Class</code>, to directly populate a Class property
- * instead of using a String class name property as bridge.
- *
- * <p>Also supports "java.lang.String[]"-style array class names,
- * in contrast to the standard <code>Class.forName</code> method.
- * Delegates to ClassUtils for actual class name resolution.
- *
- * @author Cesar Reyes
- * @since 11.12.2005
- * @see java.lang.Class#forName
- * @see org.springframework.util.ClassUtils#forName
  */
 public class CustomClassEditor<T extends BaseEntity> extends PropertyEditorSupport {
 
