b407287147d83cce05f0e8fafa36fbd833cec7e7
shoryu.christ@gmail.com <shoryu.christ@gmail.com@6f6ecb54-d94e-11dd-86c2-0f8696b7b6f9>
2009-07-07 21:51:35 +0000

    Changes to correct operation of the methods Equals and HashCode whit Hibernate

--- trunk/core/java/src/org/forzaframework/metadata/Catalog.java
+++ trunk/core/java/src/org/forzaframework/metadata/Catalog.java
@@ -103,21 +103,18 @@ public class Catalog extends BaseEntity {
 
 	public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || !(o instanceof Catalog)) return false;
 
         Catalog that = (Catalog) o;
 
-        if (code != null ? !code.equals(that.code) : that.code != null) return false;
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (code != null ? !code.equals(that.getCode()) : that.getCode() != null) return false;
+        if (name != null ? !name.equals(that.getName()) : that.getName() != null) return false;
 
         return true;
     }
 
     public int hashCode() {
-        int result;
-        result = (id != null ? id.hashCode() : 0);
-        result = 31 * result + (code != null ? code.hashCode() : 0);
+        int result = (code != null ? code.hashCode() : 0);
         result = 31 * result + (name != null ? name.hashCode() : 0);
         return result;
     }
