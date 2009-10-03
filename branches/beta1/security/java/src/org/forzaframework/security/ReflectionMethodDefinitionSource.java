56e10d1ddc8d45500241c5a25605ee5c4705f1ac
cesar@forzaframework.org <cesar@forzaframework.org@6f6ecb54-d94e-11dd-86c2-0f8696b7b6f9>
2009-10-03 17:15:37 +0000

    Correcciones al actualizar a spring 3.0

--- branches/beta1/security/java/src/org/forzaframework/security/ReflectionMethodDefinitionSource.java
+++ branches/beta1/security/java/src/org/forzaframework/security/ReflectionMethodDefinitionSource.java
@@ -16,10 +16,8 @@
 
 package org.forzaframework.security;
 
-import org.springframework.security.intercept.method.AbstractMethodDefinitionSource;
-import org.springframework.security.ConfigAttributeDefinition;
-import org.springframework.security.ConfigAttribute;
-import org.springframework.security.SecurityConfig;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
 import org.springframework.util.Assert;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.apache.commons.logging.Log;
@@ -36,88 +34,89 @@ import java.lang.reflect.Method;
  *         Date: 22/10/2008
  *         Time: 10:17:50 AM
  */
-public class ReflectionMethodDefinitionSource extends AbstractMethodDefinitionSource {
-
-    protected static Log logger = LogFactory.getLog(ReflectionMethodDefinitionSource.class);
-    private org.forzaframework.security.service.SecurityManager securityManager;
-
-    public void setSecurityManager(org.forzaframework.security.service.SecurityManager securityManager) {
-        this.securityManager = securityManager;
-    }
-
-    public ConfigAttributeDefinition getAttributes(Object object)
-        throws IllegalArgumentException {
-        Assert.notNull(object, "Object cannot be null");
-
-        if (object instanceof MethodInvocation) {
-            return this.lookupAttributes(((MethodInvocation) object));
-        }
-        return super.getAttributes(object);
-    }
-
-    protected ConfigAttributeDefinition lookupAttributes(MethodInvocation methodInvocation) {
-        try {
-            Method method = methodInvocation.getMethod();
-            logger.debug("autenticating method: " + method.getName());
-            List<String> roles = null;
-            if(method.getDeclaringClass().getName().equals("com.nopalsoft.service.Manager")){
-                if(methodInvocation.getArguments().length > 0){
-                    if(methodInvocation.getArguments()[0] instanceof Class){
-                        String targetClass = ((Class) methodInvocation.getArguments()[0]).getName();
-                        roles = securityManager.getAuthorizedRolesForMethod(method.getDeclaringClass().getName(), method.getName(), targetClass);
-                    }
-                }
-            }else{
-                roles = securityManager.getAuthorizedRolesForMethod(method.getDeclaringClass().getName(), method.getName());
-            }
-            if(roles != null && roles.size() > 0){
-            	List<ConfigAttribute> configAttributes = new ArrayList<ConfigAttribute>();
-                for(String role : roles){
-                	configAttributes.add(new SecurityConfig(role));
-                }
-                return new ConfigAttributeDefinition(configAttributes);
-            }
-            return null;
-        }
-        catch (IncorrectResultSizeDataAccessException ex) {
-            return null;
-        }
-
-    }
-
-    protected ConfigAttributeDefinition lookupAttributes(Method method) {
-        try {
-            logger.debug("autenticating method: " + method.getName());
-            List<String> roles = null;
-            if(method.getDeclaringClass().getName().equals("com.nopalsoft.service.Manager")){
+public class ReflectionMethodDefinitionSource {}
+//extends AbstractMethodDefinitionSource {
+//
+//    protected static Log logger = LogFactory.getLog(ReflectionMethodDefinitionSource.class);
+//    private org.forzaframework.security.service.SecurityManager securityManager;
+//
+//    public void setSecurityManager(org.forzaframework.security.service.SecurityManager securityManager) {
+//        this.securityManager = securityManager;
+//    }
+//
+//    public ConfigAttributeDefinition getAttributes(Object object)
+//        throws IllegalArgumentException {
+//        Assert.notNull(object, "Object cannot be null");
+//
+//        if (object instanceof MethodInvocation) {
+//            return this.lookupAttributes(((MethodInvocation) object));
+//        }
+//        return super.getAttributes(object);
+//    }
+//
+//    protected ConfigAttributeDefinition lookupAttributes(MethodInvocation methodInvocation) {
+//        try {
+//            Method method = methodInvocation.getMethod();
+//            logger.debug("autenticating method: " + method.getName());
+//            List<String> roles = null;
+//            if(method.getDeclaringClass().getName().equals("com.nopalsoft.service.Manager")){
 //                if(methodInvocation.getArguments().length > 0){
-//                    roles = securityManager.getAuthorizedRolesForMethod(method.getDeclaringClass().getName(), method.getName(), methodInvocation.getArguments()[0].getClass().getName());
+//                    if(methodInvocation.getArguments()[0] instanceof Class){
+//                        String targetClass = ((Class) methodInvocation.getArguments()[0]).getName();
+//                        roles = securityManager.getAuthorizedRolesForMethod(method.getDeclaringClass().getName(), method.getName(), targetClass);
+//                    }
 //                }
-            }else{
-                roles = securityManager.getAuthorizedRolesForMethod(method.getDeclaringClass().getName(), method.getName());
-            }
-            if(roles != null && roles.size() > 0){
-            	List<ConfigAttribute> configAttributes = new ArrayList<ConfigAttribute>();
-                for(String role : roles){
-                	configAttributes.add(new SecurityConfig(role));
-                }
-                return new ConfigAttributeDefinition(configAttributes);
-            }
-            return null;
-        }
-        catch (IncorrectResultSizeDataAccessException ex) {
-            return null;
-        }
-
-    }
-
-
-	public ConfigAttributeDefinition getAttributes(Method method,
-			Class targetClass) {
-		return null;
-	}
-
-	public Collection getConfigAttributeDefinitions() {
-		return null;
-	}
-}
+//            }else{
+//                roles = securityManager.getAuthorizedRolesForMethod(method.getDeclaringClass().getName(), method.getName());
+//            }
+//            if(roles != null && roles.size() > 0){
+//            	List<ConfigAttribute> configAttributes = new ArrayList<ConfigAttribute>();
+//                for(String role : roles){
+//                	configAttributes.add(new SecurityConfig(role));
+//                }
+//                return new ConfigAttributeDefinition(configAttributes);
+//            }
+//            return null;
+//        }
+//        catch (IncorrectResultSizeDataAccessException ex) {
+//            return null;
+//        }
+//
+//    }
+//
+//    protected ConfigAttributeDefinition lookupAttributes(Method method) {
+//        try {
+//            logger.debug("autenticating method: " + method.getName());
+//            List<String> roles = null;
+//            if(method.getDeclaringClass().getName().equals("com.nopalsoft.service.Manager")){
+////                if(methodInvocation.getArguments().length > 0){
+////                    roles = securityManager.getAuthorizedRolesForMethod(method.getDeclaringClass().getName(), method.getName(), methodInvocation.getArguments()[0].getClass().getName());
+////                }
+//            }else{
+//                roles = securityManager.getAuthorizedRolesForMethod(method.getDeclaringClass().getName(), method.getName());
+//            }
+//            if(roles != null && roles.size() > 0){
+//            	List<ConfigAttribute> configAttributes = new ArrayList<ConfigAttribute>();
+//                for(String role : roles){
+//                	configAttributes.add(new SecurityConfig(role));
+//                }
+//                return new ConfigAttributeDefinition(configAttributes);
+//            }
+//            return null;
+//        }
+//        catch (IncorrectResultSizeDataAccessException ex) {
+//            return null;
+//        }
+//
+//    }
+//
+//
+//	public ConfigAttributeDefinition getAttributes(Method method,
+//			Class targetClass) {
+//		return null;
+//	}
+//
+//	public Collection getConfigAttributeDefinitions() {
+//		return null;
+//	}
+//}
