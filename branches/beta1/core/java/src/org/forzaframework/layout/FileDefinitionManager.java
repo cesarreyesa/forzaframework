793ce2043e1e03f868648ac1cfb2f55bc727e030
shoryu.christ <shoryu.christ@6f6ecb54-d94e-11dd-86c2-0f8696b7b6f9>
2011-05-05 22:55:10 +0000

    Se agregaron a diferentes componentes la lectura de jslt

--- branches/beta1/core/java/src/org/forzaframework/layout/FileDefinitionManager.java
+++ branches/beta1/core/java/src/org/forzaframework/layout/FileDefinitionManager.java
@@ -27,4 +27,6 @@ public interface FileDefinitionManager {
 
     List<FileDefinition> getFileDefinitionsByEntityCode(String code);
 
+    List<FileDefinition> getFileDefinitionsByEntityCodes(List<String> codes);
+
 }
