42bba0461100ecf43032c264c3f25b776101097d
shoryu.christ <shoryu.christ@6f6ecb54-d94e-11dd-86c2-0f8696b7b6f9>
2011-11-12 18:59:29 +0000

    Se agrego la funcion getLastTimeInDate a DateUtils, y habilito para que las propiedades de enableDragDrop y enableDrop del grid, puedan recibir expresiones jslt

--- core/java/src/org/forzaframework/util/DateUtils.java
+++ core/java/src/org/forzaframework/util/DateUtils.java
@@ -529,4 +529,14 @@ public class DateUtils extends org.apache.commons.lang.time.DateUtils {
         return elapsed;
     }
 
+    public static Date getLastTimeInDate(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        cal.set(Calendar.MILLISECOND, 999);
+        date.setTime(cal.getTime().getTime());
+        return date;
+    }
 }
