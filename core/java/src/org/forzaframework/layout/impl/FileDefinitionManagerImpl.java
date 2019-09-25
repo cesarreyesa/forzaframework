0dc07cae9aaa8dcd70ecef81caf0c17a909a5e17
christian.tavera <christian.tavera@grupoargos.com.mx>
2019-09-25 17:52:14 -0500

    Correcciones.

--- core/java/src/org/forzaframework/layout/impl/FileDefinitionManagerImpl.java
+++ core/java/src/org/forzaframework/layout/impl/FileDefinitionManagerImpl.java
@@ -20,9 +20,7 @@ import org.forzaframework.layout.FileDefinitionManager;
 import org.forzaframework.layout.FileDefinition;
 import org.forzaframework.core.persistance.EntityManager;
 import org.hibernate.Query;
-import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 /**
@@ -45,19 +43,6 @@ public class FileDefinitionManagerImpl implements FileDefinitionManager {
         query.setParameter("entityCodeParam", code);
 
         return query.list();
-
-        //Implementacion de CriteriaBuilder de hibernate 5
-//        Session session = entityManager.getHibernateSession();
-//        CriteriaBuilder cb = session.getCriteriaBuilder();
-//        CriteriaQuery<FileDefinition> cr = cb.createQuery(FileDefinition.class);
-//        Root<FileDefinition> root = cr.from(FileDefinition.class);
-//        Predicate entityCode = cb.like(root.get("entity"), code);
-//        Predicate entityNull = cb.isNull(root.get("entity"));
-//        cr.select(root).where(cb.or(entityCode, entityNull));
-//
-//        Query<FileDefinition> query = session.createQuery(cr);
-//
-//        return query.getResultList();
     }
 
     public List<FileDefinition> getFileDefinitionsByEntityCodes(List<String> codes) {
